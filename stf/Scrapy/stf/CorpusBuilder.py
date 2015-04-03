82aa199042ec7d0aa91bc0f22ef7948ea4b33f16
mayaracm <mayaracm140@gmail.com>
2015-04-02 21:12:37 -0300

    printing only 1000 most frequent words of corpus

--- stf/Scrapy/stf/CorpusBuilder.py
+++ stf/Scrapy/stf/CorpusBuilder.py
@@ -9,14 +9,14 @@ class CorpusBuilder:
         self.corpusDict = {}
         self.text = ""
         self.stopListWords = [
-
+            "não", "sim", "são"
         ]
 
     def addWords( self, text):
         keys = self.corpusDict.keys()
         for w in re.split(r"[\s\(\),.?;:\"\']+", text):
 #            w = w.decode("utf-8").encode("iso-8859-1")
-            w = w.decode("utf-8")
+    #        w = w.decode("utf-8")
             w = w.lower()
             w = w.encode("utf-8")
             if w in keys:
@@ -30,23 +30,20 @@ class CorpusBuilder:
         if re.match( r"[\d]+", word): return 0 
         if re.match( r"qu[e(?:ai)(?:al)]", word): return 0 
         if re.match( r"d[aeio][s]?", word): return 0 
-        if word == "não": return 0 
+        if word in self.stopListWords: return 0
         if re.match( r"p[(?:ara)(?:elo)(?:or)][s]?", word): return 0 
+        if re.match( r"[\d\s\/]+", word): return 0
         return 1
 
     def print2File( self, filename):
-        print "printing to file"
         file = open( filename, 'a')
         file.seek(0)
         file.truncate()
+        i = 0
         for w in sorted(self.corpusDict, key=self.corpusDict.get, reverse=True):
-#            w = k.decode("utf-8").encode("iso-8859-1")
-#            file.write( '{0:20} ==> {1:10d}'.format( k.encode('utf8'), dic[k]) )
-            print w
             file.write( '%20s ==> %10d\n' % (w, self.corpusDict[w]))
-   #         print k
-  #          file.write( w)
-   #         file.write( str(dic[k]))
-    #        file.write( "\n\n")
+            i += 1
+            if i >= 1000:
+                break
         file.close() 
 
