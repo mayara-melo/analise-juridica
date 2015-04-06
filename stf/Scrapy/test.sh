8f46702e1c14378d951f4a077e1bc7f1cc775f11
mayaracm <mayaracm140@gmail.com>
2015-04-06 20:51:53 -0300

    add crawler test scripts

---
+++ stf/Scrapy/test.sh
@@ -0,0 +1,1 @@
+scrapy crawl -a "iDate=20010101" -a "fDate=20010501" -a "page"=1 -a "index"=1 stf --nolog
---
+++ stj/Scrapy/test.sh
@@ -0,0 +1,1 @@
+scrapy crawl -a "iDate=20010101" -a "fDate=20011015" -a "update"=0 stj --nolog
