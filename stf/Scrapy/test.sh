2ea326e40396140820edf1f114299cb2114bf5c7
Mayara Melo <mayaracm140@gmail.com>
2015-08-09 23:07:34 -0300

    organize folders and delete obsolete files

--- stf/Scrapy/test.sh
+++
@@ -1,1 +0,0 @@
-scrapy crawl -a "iDate=20010101" -a "fDate=20130101" -a "page"=1 -a "index"=1 stf --nolog
--- stj/Scrapy/test.sh
+++
@@ -1,1 +0,0 @@
-scrapy crawl -a "iDate=20010101" -a "fDate=20010305" -a "update"=0 stj --nolog
